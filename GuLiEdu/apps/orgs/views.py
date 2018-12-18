55aa48e4e2b91b223ae84a092541327f08f5ae65
shouwen <swguo12306@outlook.com>
2018-12-18 19:58:22 +0800

    add paginator class_org org_list func

--- GuLiEdu/apps/orgs/views.py
+++ GuLiEdu/apps/orgs/views.py
@@ -1,3 +1,6 @@
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import OrgInfo
@@ -8,6 +11,22 @@ from .models import CityInfo
 
 def org_list(request):
     all_orgs = OrgInfo.objects.all()
+    all_cities = CityInfo.objects.all()
+    sort_orgs = all_orgs.order_by('-love_num')[:3]
+
+    # 分页
+    pagenum = request.GET.get('pagenum', '')
+    pa = Paginator(all_orgs, 3)
+    try:
+        pages = pa.page(pagenum)
+    except PageNotAnInteger:
+        pages = pa.page(1)
+    except EmptyPage:
+        pages = pa.page(pa.num_pages)
+
     return render(request, 'org_list.html', {
-        'all_orgs': all_orgs
+        'all_orgs': all_orgs,
+        'all_cities': all_cities,
+        'pages': pages,
+        'sort_orgs': sort_orgs
     })
